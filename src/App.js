4ee94f3efbdd84e0e3ad7255a058f10177e9bad3
Douglas Purdy <dpurdy@my.wgu.edu>
2018-10-30 02:48:51 -0400

    added bootstrap styling and started components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import Contact from './components/Contact'; // eslint-disable-next-line
-import tachyons from "../node_modules/tachyons/css/tachyons.min.css";
-
+import Contact from './components/Contact'; 
+import Header from './components/Header'; // eslint-disable-next-line
+import "../node_modules/tachyons/css/tachyons.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1 className="tc bg-black white grow">Contact Manager</h1>
-       <Contact></Contact>
+      <div className="container">
+      <Header branding = "Contact Manager"/>
+        <Contact name = "John Doe" email = "John.doe@gmail.com" phone = "555-867-5309"/>
+        <Contact name = "Karen Smith" email = "karen@gmail.com" phone = "555-512-1334"/>
+      </div>
       </div>
     );
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 class Contact extends Component {
 render() {
+  const {name, email, phone} = this.props
     return (
-      <div>
-        <h1 className="bg-dark-blue grow db tc b--solid">John Doe</h1>
+      <div className="card card-body mb-3">
+        <h4 className="f4 bg-near-black white mv0 pv2 ph3">{name}</h4>
+        <div className="pa3 bt">
+        <ul className ="f6 f5-ns lh-copy measure mv0 list-group">
+          <li className ="list-group-item">Email: {email}</li>
+          <li className ="list-group-item">Phone: {phone}</li>
+        </ul>
+        </div>
       </div>
     )
   }
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+
+}
+
 export default Contact;
