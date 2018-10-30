c4e60d3caf6d96d8a8022c09dec6da9d6a6c619a
Douglas Purdy <dpurdy@my.wgu.edu>
2018-10-30 07:55:13 -0400

    added delete feature, without state manager for now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
-render() {
-  const {name, email, phone} = this.props.contact
+  state = {
+    showContactInfo: false
+  }
+  onDeleteClick = () => {
+    this.props.deleteClickHandler()
+  }
+
+  render() {
+    const { name, email, phone } = this.props.contact
+    const {showContactInfo} = this.state
     return (
       <div className="card card-body mb-3">
-        <h4 className="f4 bg-near-black white mv0 pv2 ph3">{name}</h4>
+        <h4 className="f4 bg-near-black white mv0 pv2 ph3">{name}<i onClick={()=> this.setState({ showContactInfo: !this.state.showContactInfo })} className="fas fa-sort-down pl-2" style = {{cursor : 'pointer'}} /> <i className = 'fas fa-times' style ={{ cursor: 'pointer', float:'right', color: 'red'}} onClick = {this.onDeleteClick}></i></h4>
         <div className="pa3 bt">
-        <ul className ="f6 f5-ns lh-copy measure mv0 list-group">
-          <li className ="list-group-item">Email: {email}</li>
-          <li className ="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (<ul className="f6 f5-ns lh-copy measure mv0 list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>):null }
         </div>
       </div>
     )
@@ -19,7 +27,8 @@ render() {
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
 }
 
 export default Contact;
