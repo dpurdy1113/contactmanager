837b870eb81d3a0c805da2c859a9b94684ded962
Douglas Purdy <dpurdy@my.wgu.edu>
2018-11-03 06:54:59 -0400

    added 404 page finished router work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Contacts from './components/contacts/Contacts'; 
-import AddContact from './components/contacts/AddContact'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contacts from './components/contacts/Contacts';
+import AddContact from './components/contacts/AddContact';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
-
-import {Provider} from './context';
+import { Provider } from './context';
 
 import Header from './components/layout/Header'; // eslint-disable-next-line
 import "../node_modules/tachyons/css/tachyons.min.css";
@@ -17,17 +17,17 @@ class App extends Component {
     return (
       <Provider>
         <Router>
-      <div className="App">
-      <Header branding = "Contact Manager"/>
-      <div className="container">
-      <Switch>
-        <Route exact path="/" component={Contacts}/>
-        <Route exact path="/contact/add" component={AddContact}/>
-        <Route exact path="/about" component={About}/>
-
-      </Switch>
-      </div>
-      </div>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
         </Router>
       </Provider>
     );
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="display4 text-danger text-center">404 Error! Page Not Found!</h1>
+      <p className="lead text-center"> Sorry, that page does not exist</p>
+    </div>
+  )
+}
+export default NotFound;
